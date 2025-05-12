fab53e2ded3f1985ebd529dcab2536d9fba31049
Sameer <bhandarisameer173@gmail.com>
2025-05-12 20:42:13 +0545

    COlorChangingButton

--- ButtonColorChanging.java
+++ ButtonColorChanging.java
@@ -20,3 +20,24 @@ public class ButtonColorChanging {
                 super.paintComponent(g);
                 g2.dispose();
             }
+
+            @Override
+            protected void paintBorder(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(Color.BLACK);
+                g2.setStroke(new BasicStroke(2));
+                g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+                g2.dispose();
+            }
+        };
+
+        // Style it
+        button.setFont(new Font("SansSerif", Font.BOLD, 18));
+        button.setForeground(Color.WHITE);
+        button.setPreferredSize(new Dimension(200, 60));
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false); // allow custom painting
+        button.setOpaque(false);
+
+        
